4083fda596a2647135e1c3fa355a2e161ce230a5
Felippe <felps.facul@gmail.com>
2025-03-19 15:49:17 -0300

    Create user repository

---
+++ src/main/java/com/felippestein/SpringProject/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.felippestein.SpringProject.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.felippestein.SpringProject.models.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{}
