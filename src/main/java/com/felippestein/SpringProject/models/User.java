4f4c47eb45408708bffe5c39adea3eb0f58974d0
Felippe <felps.facul@gmail.com>
2025-03-19 14:40:21 -0300

    Adicionando verificação hash code

--- src/main/java/com/felippestein/SpringProject/models/User.java
+++ src/main/java/com/felippestein/SpringProject/models/User.java
@@ -1,5 +1,7 @@
 package com.felippestein.SpringProject.models;
 
+import java.util.Objects;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -71,5 +73,32 @@ public class User {
 
     public String getPassword(){
         return this.password;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj == this)
+            return true;
+        
+        if(obj == null)
+            return false;
+        
+        if(obj instanceof User)
+            return false;
+        User other = (User) obj;
+        if(this.id == null)
+            if(other.id != null)
+                return false;
+            else if(!this.id.equals(other.id))
+                return false;
+        return Objects.equals(this.id, other.id) && Objects.equals(this.username, other.username) && Objects.equals(this.password, other.password);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
     }
 }
